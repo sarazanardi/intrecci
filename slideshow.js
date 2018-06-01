9496f820030a5dddd26a38b0f17a0b38c47e3c20
sarazanardi <37139028+sarazanardi@users.noreply.github.com>
2018-06-01 04:56:50 +0200

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,32 @@
+var image = document.getElementById('pic')
+var button = document.getElementById('show');
+
+// how to write an alert
+//$(".prev").click(function(){
+  //alert("")
+//});
+
+
+
+$(document).ready(function(){
+  $('.next').click(function(){
+    var thisImg = $('.active');
+    var nextImg = thisImg.next();
+
+    if(nextImg.length){
+      thisImg.removeClass('active');
+      nextImg.addClass('active');
+    }
+  });
+
+  $('.prev').click(function(){
+    var thisImg = $('.active');
+    var prevImg = thisImg.prev();
+
+    if(prevImg.length){
+      thisImg.removeClass('active');
+      //how to style single CSS property:
+      prevImg.addClass('active').css('z-index', 10);
+    }
+  });
+});
